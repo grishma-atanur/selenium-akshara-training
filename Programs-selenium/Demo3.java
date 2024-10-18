7218929819680569940564cf817832071fd74214
grishma-atanur <atanur.grishma@gmail.com>
2024-10-18 15:05:50 +0200

    Add files via upload

---
+++ Programs-selenium/Demo3.java
@@ -0,0 +1,24 @@
+package day04;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo3 {
+//get the current location /position of the browser
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+
+		driver.manage().window().setSize(new Dimension(500,600));
+	
+		Thread.sleep(1000);
+		
+		Point p = driver.manage().window().getPosition();
+		System.out.println(p);
+		System.out.println(p.getX());
+		System.out.println(p.getY());
+		driver.quit();
+	}
+}
